04639681d51ccde257caabc81d606bec16bc6ba2
Yuliya Karkavina <julia-barb@yandex.ru>
2021-12-06 16:29:50 +1200

    Добавить страницу

---
+++ src/views/NewPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="new-page">
+    <h1 class="new-page__title">New Page</h1>
+    <p class="new-page__conent"></p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.new-page {
+  background-color: paleturquoise;
+  height: 400px;
+
+  &__title {
+    font-size: 30px;
+  }
+
+  &__content {
+    font-style: italic;
+  }
+}
+</style>
